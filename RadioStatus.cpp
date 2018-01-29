0321c97762e037fb5f0a22446eaec8d891cccdfb
Helmut Tschemernjak <Helmut Tschemernjak@localhost>
2018-01-29 21:54:15 +0100

    Added status LEDs for MyHome revision 4

--- RadioStatus.cpp
+++ RadioStatus.cpp
@@ -40,6 +40,14 @@ MyRadioStatus::MyRadioStatus()
     ledTimeout = new DigitalOut(LED1); // green
     *ledTimeout = 0;
 #endif
+#ifdef MyHOME_BOARD_REV4
+    ledTX = new DigitalOut(LED2); // yellow
+    *ledTX = 0;
+    ledRX = new DigitalOut(STATUS_LED); // red
+    *ledRX = 0;
+    ledTimeout = new DigitalOut(LED3); // green
+    *ledTimeout = 0;
+#endif
 #ifdef __SAMD21G18A__
     inverted = true;
 #ifdef PIN_LED_TXL
