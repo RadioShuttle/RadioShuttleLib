4b68bc401f6a54118437fc12a431067385e5f8df
Helmut Tschemernjak <Helmut Tschemernjak@localhost>
2018-06-05 17:08:43 +0200

    Copied the PMRTCUtil.cpp and PinMap.h files because symlinks will break the mbed online IDE

--- examples/PMSensorRadio/PMRTCUtil.cpp
+++ examples/PMSensorRadio/PMRTCUtil.cpp
@@ -1,1 +1,258 @@
-/Volumes/Data/work/mbed-work/STM_MyHome_L4/RadioShuttleLib/examples/RadioTest/RTCUtil.cpp
+/*
+ * The file is Licensed under the Apache License, Version 2.0
+ * (c) 2018 Helmut Tschemernjak
+ * 30826 Garbsen (Hannover) Germany
+ */
+
+#ifdef ARDUINO 
+
+#include "PinMap.h"
+#include <arduino-mbed.h>
+#include <arduino-util.h>
+#if defined(ARDUINO_SAMD_ZERO) || defined(ARDUINO_ARCH_SAMD)
+#include <RTCZero.h>
+#elif ARDUINO_ARCH_ESP32
+#include <rom/rtc.h>
+#include <driver/adc.h>
+#if defined (FEATURE_SI7021) || defined (FEATURE_RTC_DS3231)
+ #include <Wire.h>
+#endif
+#ifdef FEATURE_RTC_DS3231
+ #include "ds3231.h"
+#endif
+#ifdef FEATURE_SI7021
+  #include "Adafruit_Si7021.h"
+#endif
+#endif
+#if LORA_CS != NC
+#include <sx1276-mbed-hal.h>
+#endif
+
+#ifdef FEATURE_LORA
+
+NVProperty prop; // global property store supports OTP, Flash and SRAM
+
+void InitLoRaChipWithShutdown()
+{
+#ifdef LORA_CS
+    if (LORA_CS == NC)
+      return;
+    Radio *radio = new SX1276Generic(NULL, RFM95_SX1276,
+                            LORA_SPI_MOSI, LORA_SPI_MISO, LORA_SPI_SCLK, LORA_CS, LORA_RESET,
+                            LORA_DIO0, LORA_DIO1, LORA_DIO2, LORA_DIO3, LORA_DIO4, LORA_DIO5);
+    RadioEvents_t radioEvents;
+    memset(&radioEvents, 0, sizeof(radioEvents));
+    if (radio->Init(&radioEvents)) {
+        radio->Sleep();
+        delete radio;
+    }
+#endif
+}
+
+bool ESP32DeepsleepWakeup;
+
+#if defined(ARDUINO_SAMD_ZERO) || defined(ARDUINO_ARCH_SAMD)
+
+#ifdef BOOSTER_EN50
+DigitalOut boost50(BOOSTER_EN50);
+#endif
+#ifdef BOOSTER_EN33
+DigitalOut boost33(BOOSTER_EN33);
+#endif
+#ifdef DISPLAY_EN
+DigitalOut displayEnable(DISPLAY_EN);
+#endif
+
+RTCZero rtc;
+
+void alarmMatch()
+{
+  int interval = 5;
+  int secs = rtc.getSeconds() + interval;
+  if (secs >= 58)
+    secs = interval;
+  rtc.setAlarmSeconds(secs);
+  rtc.enableAlarm(rtc.MATCH_SS);
+}
+
+void RTCInit(const char *date, const char *timestr)
+{
+  rtc.begin();
+  rtc.attachInterrupt(alarmMatch);
+  alarmMatch();
+  time_t t = cvt_date(date, timestr);
+  if (rtc.getYear() == 0 || rtc.getEpoch() < (uint32_t)t) {
+    rtc.setEpoch(t);
+  }
+  setTickerStartSecs(rtc.getSeconds() + (rtc.getMinutes()*60) + rtc.getHours() * 3600);
+  dprintf("RTC Clock: %d/%d/%d %02d:%02d:%02d", rtc.getDay(), rtc.getMonth(), rtc.getYear() + 2000, rtc.getHours(), rtc.getMinutes(), rtc.getSeconds());
+
+  uint8_t reason = PM->RCAUSE.reg;
+  if (reason & (PM_RCAUSE_WDT | PM_RCAUSE_BOD33)) {
+    dprintf("Boot: CPU(D21) reset by: %s %s",
+      PM->RCAUSE.reg & PM_RCAUSE_WDT ? "WatchDog" : "",
+      PM->RCAUSE.reg & PM_RCAUSE_BOD33 ? "Brown-Out" : "");
+  }
+#ifdef BOOSTER_EN50
+  boost50 = 0;
+  boost33 = 0;
+#endif
+#ifdef DISPLAY_EN
+  displayEnable = 1; // disconnects the display from the 3.3 power
+#endif
+  InitWatchDog();
+}
+
+#elif ARDUINO_ARCH_ESP32 
+
+#ifdef FEATURE_SI7021
+Adafruit_Si7021 *sensorSI7021;  
+#endif
+
+#ifdef ESP32_ECO_POWER_REV_1
+float GetBatteryVoltage()
+{
+#ifdef EXT_POWER_SW
+  DigitalOut extPWR(EXT_POWER_SW);
+  extPWR = EXT_POWER_ON;
+#endif
+  float volt;
+  float vref = (float)prop.GetProperty(prop.ADC_VREF, 1100)/1000.0;
+  
+  wait_ms(1);
+  analogSetPinAttenuation(BAT_POWER_ADC, ADC_0db); //  1.124 Volt ID 132, 
+  wait_ms(20);  
+  
+  int value = 0;
+  for (int i = 0; i < 12; i++) {
+    value += analogRead(BAT_POWER_ADC); // BAT_POWER_ADC is ADC1
+  }
+  value /= 12;
+  float voltstep = vref/(float)(1<<12); // 12 bit 4096
+  volt = (float) ((value * voltstep) / 82) * (82+220); // 82k, 82k+220k
+  volt -= 0.065; // correction in millivolts
+  dprintf("Power: %.2fV (ADC: %d Vref: %.3f)", volt, value, vref); 
+
+#ifdef EXT_POWER_SW
+  extPWR = EXT_POWER_OFF; 
+  DigitalIn tmpPWR(EXT_POWER_SW);
+  tmpPWR.mode(PullUp); // turn off the power, PullUp will keep it off in deepsleep
+#endif
+  return volt;
+}
+
+#endif
+
+RTC_DATA_ATTR int bootCount = 0;
+RTC_DATA_ATTR bool hasSensor;
+
+
+void RTCInit(const char *date, const char *timestr)
+{
+#if defined (FEATURE_SI7021) || defined (FEATURE_RTC_DS3231)
+  Wire.begin();
+#endif
+
+  time_t now = time(NULL);
+  
+#ifdef FEATURE_RTC_DS3231
+  DS3231_init(DS3231_CONTROL_INTCN);
+  struct ts ds;
+  DS3231_get(&ds);
+  if (!now || now < ds.unixtime || now > ds.unixtime) {
+    time_t t = cvt_date(date, timestr);
+    if (!ds.unixtime || t > ds.unixtime) {
+      struct tm *tmp = gmtime(&t);
+      ds.sec = tmp->tm_sec;
+      ds.min = tmp->tm_min;
+      ds.hour = tmp->tm_hour;
+      ds.wday = tmp->tm_wday;
+      ds.mday = tmp->tm_mday;
+      ds.mon = tmp->tm_mon + 1;
+      ds.year =  tmp->tm_year + 1900;
+      DS3231_set(ds);
+      DS3231_get(&ds);    
+#if 0  
+      dprintf("%02d:%02d:%02d wday(%d) mday(%d) mon(%d) year(%d), ", ds.hour, ds.min, ds.sec, ds.wday, ds.mday, ds.mon, ds.year);
+      dprintf("RTC: %.2f°C", DS3231_get_treg());
+      time_t now = ds.unixtime;
+      dprintf("ctime: %s", ctime(&now));
+#endif
+    }
+    struct timeval tv;
+    memset(&tv, 0, sizeof(tv));
+    tv.tv_sec = ds.unixtime;;
+    struct timezone tz;
+    memset(&tz, 0, sizeof(tz));
+    tz.tz_minuteswest = 0;
+    tz.tz_dsttime = 0;
+    settimeofday(&tv, &tz);    
+    dprintf("RTC Clock: %d/%d/%d %02d:%02d:%02d", ds.mday, ds.mon, ds.year, ds.hour, ds.min, ds.sec);
+  }
+  int i = prop.GetProperty(prop.RTC_AGING_CAL, 0);
+  if (i && i != DS3231_get_aging())
+    DS3231_set_aging(i);
+#else // without ds3231
+  if (!now)) {
+    time_t t = cvt_date(date, timestr);
+    struct timeval tv;
+    memset(&tv, 0, sizeof(tv));
+    tv.tv_sec = t;
+    struct timezone tz;
+    memset(&tz, 0, sizeof(tz));
+    tz.tz_minuteswest = 0;
+    tz.tz_dsttime = 0;
+    settimeofday(&tv, &tz);
+  }
+#endif
+
+  esp_sleep_wakeup_cause_t wakeup_reason = esp_sleep_get_wakeup_cause();
+  if (wakeup_reason)
+    dprintf("Boot: %s (bootCount: %d)", ESP32WakeUpReason(wakeup_reason), ++bootCount);
+  dprintf("Boot: CPU(Pro): %s", ESP32ResetReason(rtc_get_reset_reason(0)));
+  dprintf("Boot: CPU(App): %s", ESP32ResetReason(rtc_get_reset_reason(1)));
+  dprintf("ESP32: Revision: %d (%d MHz)", ESP.getChipRevision(), ESP32Frequency()/1000000);
+
+
+  if (wakeup_reason == ESP_SLEEP_WAKEUP_UNDEFINED) { 
+    /*
+     * Run this only on reset boot, not from deepsleep.
+     */
+#ifdef EXT_POWER_SW
+    DigitalIn extPWR(EXT_POWER_SW);
+    extPWR.mode(PullUp); // turn off the power, PullUp will keep it off in deepsleep
+#endif
+#ifdef BAT_POWER_ADC
+    GetBatteryVoltage(); 
+#endif
+
+#ifdef FEATURE_SI7021
+  sensorSI7021 = new Adafruit_Si7021();
+  if (sensorSI7021->begin()) {
+      hasSensor = true;
+      dprintf("%s: Rev(%d)  %.2f°C  Humidity: %.2f%%", sensorSI7021->model, sensorSI7021->revision, sensorSI7021->readTemperature(), sensorSI7021->readHumidity());
+  } else {
+    delete sensorSI7021;
+    sensorSI7021 = NULL;
+  }
+#endif
+  } else {
+    /* 
+     *  runs on deepsleep wakeup
+     */
+     ESP32DeepsleepWakeup = true;
+ #ifdef FEATURE_SI7021
+    if (hasSensor) {
+       sensorSI7021 = new Adafruit_Si7021();
+       dprintf("%s: %.2f°C  Humidity: %.2f%%", sensorSI7021->model, sensorSI7021->readTemperature(), sensorSI7021->readHumidity());
+     }
+#endif
+  }
+}
+
+#else
+#error "Unkown platform"
+#endif
+#endif // FEATURE_LORA
+#endif // ARDUINO 
+
--- examples/PMSensorRadio/PinMap.h
+++ examples/PMSensorRadio/PinMap.h
@@ -1,1 +1,184 @@
-/Volumes/Data/work/mbed-work/STM_MyHome_L4/RadioShuttleLib/examples/RadioTest/PinMap.h
+/*
+ * The file is Licensed under the Apache License, Version 2.0
+ * (c) 2018 Helmut Tschemernjak
+ * 30826 Garbsen (Hannover) Germany
+ */
+#ifdef ARDUINO 
+#define RS_MAJOR    2
+#define RS_MINOR    4
+
+#ifdef ARDUINO_SAMD_ATMEL_SAMD21_XPRO_V1
+
+#define FEATURE_LORA	1
+
+#define	SW0				3		// switch needs pullup.
+#define LED       LED_BUILTIN
+
+#define MYSERIAL          Serial
+
+#define LORA_SPI_MOSI   PIN_SPI_MOSI	// PA06 
+#define LORA_SPI_MISO   PIN_SPI_MISO	// PA04
+#define LORA_SPI_SCLK   PIN_SPI_SCK		// PA07
+#define LORA_CS			    PIN_SPI_SS		// PA05
+#define LORA_RESET      PIN_A0				// PB00
+#define LORA_DIO0       PIN_A1	      // PB01 used for Rx, Tx Interrupt 	
+#define LORA_DIO1       PIN_A2	  		// PA10 Fifo Level/Full, RxTimeout/Cad Detection Interrupt, unused in RadioShuttle
+#define LORA_DIO2       PIN_A3	      // PA11 FhssChangeChannel when FreqHop is on (unused in RadioShuttle)
+#define LORA_DIO3       PIN_A4	      // PA02 used Cad Detection in RS_Node_Offline/Checking mode
+#define LORA_DIO4       PIN_A5        // PA03 FSK mode preamble detected, unused in RadioShuttle
+#define LORA_DIO5       NC		        // NC   FSK mode ready / ClockOut, unused in RadioShuttle
+
+#elif __SAMD21G18A__ // Zero
+#define FEATURE_LORA  1
+
+// #define D21_LONGRA_REV_200  1		// board with Lipo power supply/charger, mini USB
+// #define D21_LONGRA_REV_301  1		// board with Lipo power supply/charger, micro USB
+// #define D21_LONGRA_REV_630  1		// board with Lipo power supply/charger, micro USB
+#define D21_LONGRA_REV_720  1		// Maker Faire Hannover revision, micro USB
+
+
+
+#if defined(D21_LONGRA_REV_301) || defined(D21_LONGRA_REV_200)
+
+#define SW0      		  1   // switch needs pullup.
+#define LED       		0
+#define MYSERIAL    	SerialUSB
+
+#define LORA_SPI_MOSI   PIN_SPI_MOSI  // PA06?
+#define LORA_SPI_MISO   PIN_SPI_MISO  // PA04?
+#define LORA_SPI_SCLK   PIN_SPI_SCK   // PA07?
+#define LORA_CS         2             // PA05?
+#define LORA_RESET      38            // PB00?
+#define LORA_DIO0       11            // used for Rx, Tx Interrupt
+#define LORA_DIO1       13            // Fifo Level/Full, RxTimeout/Cad Detection Interrupt, unused in RadioShuttle
+#define LORA_DIO2       10            // FhssChangeChannel when FreqHop is on, unused in RadioShuttle
+#define LORA_DIO3       3             // used Cad Detection in RS_Node_Offline/Checking mode
+#define LORA_DIO4       5             // FSK mode preamble detected, unused in RadioShuttle
+#define LORA_DIO5       NC            // FSK mode ready / ClockOut, unused in RadioShuttle
+
+#elif defined (D21_LONGRA_REV_630) || defined (D21_LONGRA_REV_720)
+
+#define SW0       		12              // PA19 switch needs pullup
+#define LED       		LED_BUILTIN     // PA17
+#define MYSERIAL    	SerialUSB
+
+#define LORA_SPI_MOSI   PIN_SPI_MOSI  // PB10
+#define LORA_SPI_MISO   PIN_SPI_MISO  // PA12
+#define LORA_SPI_SCLK   PIN_SPI_SCK   // PB11
+#define LORA_CS         3             // PA09
+#define LORA_RESET      38            // PA13
+#define LORA_DIO0       16            // PB09 used for Rx, Tx Interrupt
+#define LORA_DIO1       5             // PA15 Fifo Level/Full, RxTimeout/Cad Detection Interrupt, unused in RadioShuttle
+#define LORA_DIO2       2             // PA14 FhssChangeChannel when FreqHop is on, unused in RadioShuttle
+#define LORA_DIO3       18            // PA05 used Cad Detection in RS_Node_Offline/Checking mode
+#define LORA_DIO4       17            // PA04 FSK mode preamble detected, unused in RadioShuttle
+#define LORA_DIO5       NC            //      FSK mode ready / ClockOut, unused in RadioShuttle
+
+#define BOOSTER_EN33    9             // Enable 3.3 volt 150mA max
+#define BOOSTER_EN50    8             // Enable 5.0 volt 150mA max
+#define DISPLAY_EN      4             // Turn on display power (3.3 V must be enabled first)
+
+
+#elif defined(ARDUINO_SAMD_FEATHER_M0) // Feather M0 w/Radio
+
+#define SW0           12              // switch needs pullup, must be conected to the headers
+#define LED           LED_BUILTIN     // 13
+#define MYSERIAL      Serial          // this is a USB Serial, however the Feather M0 calls it only Serial.
+
+#define LORA_SPI_MOSI   PIN_SPI_MOSI  // PA12
+#define LORA_SPI_MISO   PIN_SPI_MISO  // PB10
+#define LORA_SPI_SCLK   PIN_SPI_SCK   // PB11
+#define LORA_CS         8             // PA06
+#define LORA_RESET      4             // PA08
+#define LORA_DIO0       3             // PA09 used for Rx, Tx Interrupt, Cad Detection
+#define LORA_DIO1       NC            // Fifo Level/Full, RxTimeout/Cad Detection Interrupt, unused in RadioShuttle
+#define LORA_DIO2       NC            // FhssChangeChannel when FreqHop is on, unused in RadioShuttle
+#define LORA_DIO3       NC            // used Cad Detection in RS_Node_Offline/Checking mode
+#define LORA_DIO4       NC            // FSK mode preamble detected, unused in RadioShuttle
+#define LORA_DIO5       NC            // FSK mode ready / ClockOut, unused in RadioShuttle
+
+
+/*
+ * PIN_LED_RXL and PIN_LED_TXL are already defined.
+ */
+#else
+
+#error "Unkown D21 board revision"
+
+#endif
+
+#elif defined(ARDUINO_ESP32_DEV)      // the ESP32 ECO Power Boards (with and without LoRa)
+
+#define ESP32_ECO_POWER_REV_1            // our rev1 ESP32 ECO Power Boards 
+
+#ifdef ESP32_ECO_POWER_REV_1
+#define ESP32_ECO_POWER   
+#define FEATURE_LORA  1
+
+#define SW0           0               // uses hardware pullup
+#define LED           2               // green LED
+#define LED2          12              // red LED
+#define MYSERIAL      Serial          // the a regular serial IO1_TXD0/IO3_RXD0
+#define FEATURE_RTC_DS3231            // an I2C clock.
+#define FEATURE_SI7021                // Temperature & Humidity add-on sensor
+
+#define LORA_SPI_MOSI   MOSI          // MOSI 23 Arduino-Dev
+#define LORA_SPI_MISO   MISO          // MISO 19 Arduino-Dev
+#define LORA_SPI_SCLK   SCK           // SCK  18 Arduino-Dev
+#define LORA_CS         5             // LORA_DEFAULT_SS_PIN 
+#define LORA_RESET      17            // LORA_DEFAULT_RESET_PIN
+#define LORA_DIO0       16            // LORA_DEFAULT_DIO0_PIN
+#define LORA_DIO1       NC            // Fifo Level/Full, RxTimeout/Cad Detection Interrupt, unused in RadioShuttle
+#define LORA_DIO2       NC            // FhssChangeChannel when FreqHop is on, unused in RadioShuttle
+#define LORA_DIO3       NC            // used Cad Detection in RS_Node_Offline/Checking mode
+#define LORA_DIO4       NC            // FSK mode preamble detected, unused in RadioShuttle
+#define LORA_DIO5       NC            // FSK mode ready / ClockOut, unused in RadioShuttle
+
+#define RTC_I2C_SDA     SDA           // DS3231 RTC Chip
+#define RTC_I2C_SDL     SDL           //
+#define RTC_INTR        SWO           // DS3231 RTC interrupt sharred with the switch
+
+#define EXT_POWER_SW    15            // Switch VDD on pin JP10
+#define EXT_POWER_ON    0
+#define EXT_POWER_OFF   1
+
+#define BAT_POWER_ADC   35            // uses a 1.0
+
+#else
+
+#define SW0           0               // no pullup
+#define LED           2               // red LED
+#define MYSERIAL      Serial          // 
+
+#endif
+
+#elif defined(ARDUINO_Heltec_WIFI_LoRa_32)   // the Heltec Board
+#define FEATURE_LORA  1
+
+#define SW0           0               // no pullup, TODO check setup code
+#define LED           25               // 
+#define MYSERIAL      Serial          // this is a USB Serial, however the Feather M0 calls it only Serial.
+
+#define LORA_SPI_MOSI   MOSI          // MOSI 27 Heltec, 23 Arduino-Dev
+#define LORA_SPI_MISO   MISO          // MISO 19 Heltec, 19 Arduino-Dev
+#define LORA_SPI_SCLK   SCK           // SCK   5 Heltec, 18 Arduino-Dev
+#define LORA_CS         18            // LORA_DEFAULT_SS_PIN 
+#define LORA_RESET      14            // LORA_DEFAULT_RESET_PIN
+#define LORA_DIO0       26            // LORA_DEFAULT_DIO0_PIN
+#define LORA_DIO1       NC            // Fifo Level/Full, RxTimeout/Cad Detection Interrupt, unused in RadioShuttle
+#define LORA_DIO2       NC            // FhssChangeChannel when FreqHop is on, unused in RadioShuttle
+#define LORA_DIO3       NC            // used Cad Detection in RS_Node_Offline/Checking mode
+#define LORA_DIO4       NC            // FSK mode preamble detected, unused in RadioShuttle
+#define LORA_DIO5       NC            // FSK mode ready / ClockOut, unused in RadioShuttle
+
+#define DISPLAY_ADDRESS 0x3c
+#define DISPLAY_SDA     4
+#define DISPLAY_SCL     15
+#define DISPLAY_RESET   16
+
+#else 
+#error "unkown board"
+#endif
+
+#endif // Arduino
+
